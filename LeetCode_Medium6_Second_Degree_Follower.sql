d161fc954e940b006c2091cef7c083cb9c617e6e
freena <freena22@gmail.com>
2018-07-14 21:37:00 -0700

    add two more medium questions

---
+++ LeetCode_Medium6_Second_Degree_Follower.sql
@@ -0,0 +1,42 @@
+# LeetCode_Medium6_Second_Degree_Follower
+
+# Medium
+
+/*
+In facebook, there is a follow table with two columns: followee, follower.
+
+Please write a sql query to get the amount of each follower’s follower if he/she has one.
+
+For example:
+
++-------------+------------+
+| followee    | follower   |
++-------------+------------+
+|     A       |     B      |
+|     B       |     C      |
+|     B       |     D      |
+|     D       |     E      |
++-------------+------------+
+should output:
++-------------+------------+
+| follower    | num        |
++-------------+------------+
+|     B       |  2         |
+|     D       |  1         |
++-------------+------------+
+Explaination:
+Both B and D exist in the follower list, when as a followee, B's follower is C and D, and D's follower is E. A does not exist in follower list.
+Note:
+Followee would not follow himself/herself in all cases.
+Please display the result in follower's alphabet order.
+
+*/
+
+
+SELECT
+  a.follower,
+  COUNT(DISTINCT b.follower) AS num
+FROM follow a
+JOIN follow b ON a.follower = b.followee
+GROUP BY a.follower;
+
