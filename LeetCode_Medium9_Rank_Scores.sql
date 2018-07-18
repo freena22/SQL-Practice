06aedf78d7b1bf313c3f10597e0cbb67046199c0
freena <freena22@gmail.com>
2018-07-17 18:52:44 -0700

    add three more questions

---
+++ LeetCode_Medium9_Rank_Scores.sql
@@ -0,0 +1,39 @@
+# LeetCode_Medium9_Rank_Scores
+
+# Medium
+
+/*
+
+Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+
++----+-------+
+| Id | Score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+For example, given the above Scores table, your query should generate the following report (order by highest score):
+
++-------+------+
+| Score | Rank |
++-------+------+
+| 4.00  | 1    |
+| 4.00  | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.50  | 4    |
++-------+------+
+*/
+
+
+
+SELECT s1.Score, COUNT(DISTINCT s2.Score) Rank
+FROM Scores s1 JOIN Scores s2 ON s1.Score <= s2.Score
+GROUP BY s1.Id
+ORDER BY s1.Score DESC;
+
